4cced4ba7838beb04d1a78f29b30bd9984e47f17
Taeyong Lee <d104601@hotmail.com>
2022-05-23 23:54:28 -0400

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var cityFormEl = document.querySelector('#city-form');
+var cities = [];
+var cityFormEl = document.querySelector('#cityForm');
 var historyEl = document.querySelector('#history');
 var inputEl = document.querySelector('#city');
 
@@ -6,8 +7,25 @@ var submitButton = function(event) {
     event.preventDefault();
 
     var cityName = inputEl.value.trim();
-    addHistory();
-    getResult();
+    if(cityName)
+    {
+        if(cities.includes(cityName))
+        {
+            cities.splice(cities.indexOf(cityName));
+        }
+        cities.unshift(cityName);
+
+        while(cities.length > 8)
+        {
+            cities.pop();
+        }
+        addHistory();
+        getResult(cityName);
+
+    }
+    else{
+        window.alert("Enter a city name.");
+    }
 }
 
 var historyButton = function(event) {
@@ -16,25 +34,52 @@ var historyButton = function(event) {
 
 }
 
+
 // function to get search result as city name
 function getResult(city) {
-    var apiKey = '95827148da73fc51bf98de54195cc14e';
-    var apiUrl = 'api.openweathermap.org/data/2.5/weather?q={'+ city + '}&appid=' + apiKey;
+    const apiKey = '95827148da73fc51bf98de54195cc14e';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=' + apiKey;
 
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayResult(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      });
+}
+
+function displayResult(data) {
+    console.log(data);
 }
 
 // function to add search history as clicking search button
-function addHistory(city) {
-
+function addHistory() {
+    localStorage.setItem("cities", JSON.stringify(cities));
     getHistory();
 }
 
 // function to get search history from local storage.
 function getHistory() {
+    historyEl.textContent = "";
+    if(localStorage.getItem("cities") !== null)
+    {
+        cities = JSON.parse(localStorage.getItem("cities"));
+    }
 
+    for(let i = 0; i < cities.length; i++)
+    {
+        var historyButton = document.createElement("button");
+        historyButton.textContent = cities[i];
+        historyButton.setAttribute("class", "btn btn-secondary btn-block");
+        historyButton.setAttribute("name", cities[i]);
+        historyButton.setAttribute("type", "submit");
+        historyEl.append(historyButton);
+    }
 }
 
-
-getHistory()
+getHistory();
 cityFormEl.addEventListener('submit', submitButton);
 historyEl.addEventListener('click', historyButton);
